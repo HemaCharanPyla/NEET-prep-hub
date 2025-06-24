fe42e3c1eaf4275b181e3c9a3bbbae4acdd49b4e
HemaCharanPyla <hemacharanpyla@gmail.com>
2025-06-24 21:13:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,5 +22,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(NEET Prep Hub server running on port ${PORT});
-}); 
+    console.log(`NEET Prep Hub server running on port ${PORT}`);
+});
